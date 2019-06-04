cf8f9440bea3a4fc947a5a3310eab49178264330
harinder <harinderpalkhakh@gmail.com>
2019-06-03 21:30:35 -0700

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	const float Y_ANGLE_MIN = 0.0f;
+	const float Y_ANGLE_MAX = 50.0f;
+
+	public GameObject player;
+	float cameraDistance = 10.0f;
+	float positionX = 0.0f;
+	float positionY = 0.0f;
+	float rotationSpeed = 4.0f;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update () {
+		positionX += Input.GetAxis ("Mouse X") * rotationSpeed;
+		positionY += Input.GetAxis ("Mouse Y") * rotationSpeed;
+		positionY = Mathf.Clamp (positionY, Y_ANGLE_MIN, Y_ANGLE_MAX);
+	}
+
+	void LateUpdate () {
+		Vector3 dir = new Vector3 (0, 0, -cameraDistance);
+		Quaternion rotation = Quaternion.Euler (positionY, positionX, 0.0f);
+		transform.position = player.transform.position + rotation * dir;
+		transform.LookAt (player.transform);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	
+//	const float SPEED_MIN = -1000.0f;
+//	const float SPEED_MAX = 1000.0f;
+//
+//	public Transform relativeTransform;
+//	public float speed = 10;
+//	public Vector3 jumpMovement = new Vector3 (0,10,0);
+//
+//	private Rigidbody rb;
+//	private bool onFloor = false;
+//
+//	// Use this for initialization
+//	void Start() {
+//		rb = GetComponent<Rigidbody> ();
+//	}
+//	
+//	// Update is called once per frame
+//	void Update() {
+//
+//	}
+//
+//	void FixedUpdate() {
+//		Vector3 moveDirection = Vector3.zero;
+//
+//		if (Input.GetKey (KeyCode.W)) {
+//			moveDirection += relativeTransform.forward;
+//		}
+//		if (Input.GetKey (KeyCode.S)) {
+//			moveDirection -= relativeTransform.forward;
+//		}
+//		if (Input.GetKey (KeyCode.A)) {
+//			moveDirection -= relativeTransform.right;
+//		}
+//		if (Input.GetKey (KeyCode.D)) {
+//			moveDirection += relativeTransform.right;
+//		}
+//
+//		moveDirection.y = 0.0f;
+//
+//		if (onFloor) {
+//			rb.AddForce (moveDirection.normalized * speed);
+//			rb.velocity = new Vector3 (Mathf.Clamp (rb.velocity.x, SPEED_MIN, SPEED_MAX), Mathf.Clamp (rb.velocity.y, SPEED_MIN, SPEED_MAX), Mathf.Clamp (rb.velocity.z, SPEED_MIN, SPEED_MAX));
+//		}
+//
+//		if (Input.GetKeyDown("space") && onFloor) {
+//			rb.AddForce (jumpMovement);
+//		}
+//		Debug.Log (onFloor);
+//	}
+//
+//	void OnCollisionEnter (Collision other) {
+//		onFloor = true;
+//	}
+//
+//	void OnCollisionExit (Collision other) {
+//		onFloor = false;
+//	}
+	public float speed = 10;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+}
